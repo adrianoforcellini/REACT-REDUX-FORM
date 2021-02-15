58b11e29ecc384f6547b6dca49688ecbbe96563b
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-02-15 17:11:05 -0300

    rerererererecomeçando

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import Header from './components/header';
+import Fieldset1 from './components/fieldset1'
+import Fieldset2 from './components/fieldset2';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ <Header/>
+    <Fieldset1/>
+    <Fieldset2/>
     </div>
   );
 }
