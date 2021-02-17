d2bc29e71b6055227584f5cd6bcf70c8d7f5041c
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-02-17 11:03:14 -0300

    form quase pronto.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import Header from './components/header';
 import Fieldset1 from './components/fieldset1'
 import Fieldset2 from './components/fieldset2';
 import './App.css';
+import Button from './components/button';
+import { Provider } from "react-redux";
+import store from "./store";
+
+// import { Provider } from 'react-redux';
+// import store from './store/index';
 
 function App() {
   return (
     <div className="App">
- <Header/>
-    <Fieldset1/>
-    <Fieldset2/>
+          <Provider store={store}>
+
+      <Header />
+      <Fieldset1 />
+      <Fieldset2 />
+      <Button />
+      </Provider>
+
     </div>
   );
 }
